7bdc253a3b46fdfeaac876e550981ef4c78ce626
abebed19 <abebed19@gmail.com>
2026-03-09 12:03:21 +0300

    Stack trace

---
+++ Statistics.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package validating;
+
+import java.util.List;
+
+/**
+ *
+ * @author aaron
+ */
+public class Statistics {
+    private List<Integer> numbers;
+    
+    public void add(int number){
+        this.numbers.add(number);
+    }
+    public  double average(){
+       return  numbers.stream()
+                .mapToInt(i->i)
+                .average()
+                .getAsDouble();
+                
+    }
+    
+}
